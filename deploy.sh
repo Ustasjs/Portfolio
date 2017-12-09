b0afa4426614dec908d23b867143c381c12839d8
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2017-12-09 22:11:07 +0300

    fix deploy config

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ set -e
 zip -r build.zip build
 
 curl -H "Content-type: application/zip" \
-     -H "Authorization: Bearer $CRYPTO" \
+     -H "Authorization: Bearer $ENV_TOKEN" \
      --data-binary "@build.zip" \
      https://api.netlify.com/api/v1/sites
