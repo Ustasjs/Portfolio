f90c08259c19463cfb09d70f02a87ddbebed499e
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2017-12-22 18:48:01 +0300

    phone is ready

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,3 +5,5 @@ import './index.scss';
 import '../../components/user/user.scss';
 import '../../components/socials/socials.scss';
 import '../../components/nav_list/nav_list.scss';
+// media
+import '../../static/styles/layout/settingsMedia.scss';
