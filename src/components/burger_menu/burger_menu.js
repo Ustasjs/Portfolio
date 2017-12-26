9a8ca531be0026da9d59cbe2bb2f4045901ffdbb
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2017-12-26 13:09:33 +0300

    flip is ready

--- src/components/burger_menu/burger_menu.js
+++ src/components/burger_menu/burger_menu.js
@@ -1,24 +1,26 @@
 class Burger {
   init() {
-    let burgers = document.querySelectorAll('.burger');
-    let burgerHeader = document.querySelector('.burger_header');
-    let modal = document.getElementById('modal');
-    let leftPart = modal.querySelector('.modal__left');
-    let rightPart = modal.querySelector('.modal__right');
+    const burgers = document.querySelectorAll('.burger');
+    const burgerHeader = document.querySelector('.burger_header');
+    const modal = document.getElementById('modal');
+    const leftPart = modal.querySelector('.modal__left');
+    const rightPart = modal.querySelector('.modal__right');
 
-    [].forEach.call(burgers, (elem) => {
-      elem.addEventListener('click', () => {
-        modal.classList.toggle('modal_active');
-        burgerHeader.classList.toggle('burger_cross');
-        document.body.classList.toggle('prevent-scroll');
+    if (burgers && burgerHeader && modal && leftPart && rightPart) {
+      [].forEach.call(burgers, (elem) => {
+        elem.addEventListener('click', () => {
+          modal.classList.toggle('modal_active');
+          burgerHeader.classList.toggle('burger_cross');
+          document.body.classList.toggle('prevent-scroll');
 
-        setTimeout(() => {
-          leftPart.classList.toggle('modal__left_active');
-          rightPart.classList.toggle('modal__right_active');
-        }, 0)
+          setTimeout(() => {
+            leftPart.classList.toggle('modal__left_active');
+            rightPart.classList.toggle('modal__right_active');
+          }, 0)
 
+        })
       })
-    })
+    }
   }
 }
 
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,3 +7,7 @@ import '../../components/socials/socials.scss';
 import '../../components/nav_list/nav_list.scss';
 // media
 import '../../static/styles/layout/settingsMedia.scss';
+// scripts
+import flip from '../../scripts/auth_flip.js'
+
+flip.init();
