f7f5afc608805dade91f8465da0026c6c158b947
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2017-12-30 20:10:57 +0300

    add load image error handling

--- src/components/preloader/preloader.js
+++ src/components/preloader/preloader.js
@@ -34,20 +34,23 @@ class Preloader {
   loadImages(images) {
     const step = Math.ceil(100 / images.length);
 
+    function loadAndErrorHandler(resolve) {
+      this.persentage = this.persentage + step;
+      if (this.persentage > 100) {
+        this.persentage = 100
+      }
+      this.svgText.textContent = `${this.persentage}%`;
+
+      if (this.persentage === 100) {
+        resolve()
+      }
+    }
+
     return new Promise((resolve) => {
 
       [].forEach.call(images, (elem) => {
-        elem.addEventListener('load', () => {
-          this.persentage = this.persentage + step;
-          if (this.persentage > 100) {
-            this.persentage = 100
-          }
-          this.svgText.textContent = `${this.persentage}%`;
-
-          if (this.persentage === 100) {
-            resolve()
-          }
-        })
+        elem.addEventListener('load', loadAndErrorHandler.bind(this, resolve))
+        elem.addEventListener('error', loadAndErrorHandler.bind(this, resolve))
       })
     })
   }
