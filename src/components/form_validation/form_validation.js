b67f84c0d03f86c7f1ca175e4a805f516e178afe
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2018-01-08 16:01:13 +0300

    improve form validation

--- src/components/form_validation/form_validation.js
+++ src/components/form_validation/form_validation.js
@@ -5,6 +5,7 @@ class FormValidation {
     const contactForm = document.getElementById('contacts');
 
     if (authForm) {
+      const containerAuth = document.querySelector('.header');
       const login = document.getElementById('login');
       const password = document.getElementById('password');
 
@@ -13,15 +14,16 @@ class FormValidation {
         removeAllErrors('auth');
 
         if (login.value === '' && password.value === '') {
-          this.handleAllInputsSubmit('auth', [login, password]);
+          this.handleAllInputsSubmit(containerAuth, 'error_auth', [login, password]);
         } else {
-          this.handleOneInputSubmit('auth', login);
-          this.handleOneInputSubmit('auth', password);
+          this.handleOneInputSubmit(containerAuth, 'error_auth', login);
+          this.handleOneInputSubmit(containerAuth, 'error_auth', password);
         }
       })
     }
 
     if (contactForm) {
+      const containerContact = document.querySelector('.feedback');
       const name = document.getElementById('name');
       const email = document.getElementById('email');
       const message = document.getElementById('message');
@@ -31,11 +33,11 @@ class FormValidation {
         removeAllErrors('contact');
 
         if (name.value === '' && email.value === '' && message.value === '') {
-          this.handleAllInputsSubmit('contact', [name, email, message]);
+          this.handleAllInputsSubmit(containerContact, 'error_contact', [name, email, message]);
         } else {
-          this.handleOneInputSubmit('contact', name);
-          this.handleOneInputSubmit('contact', email);
-          this.handleOneInputSubmit('contact', message);
+          this.handleOneInputSubmit(containerContact, 'error_contact', name);
+          this.handleOneInputSubmit(containerContact, 'error_contact', email);
+          this.handleOneInputSubmit(containerContact, 'error_contact', message);
         }
       })
     }
@@ -56,51 +58,47 @@ class FormValidation {
     return input.value === '';
   }
 
-  handleOneInputSubmit(typeOfForm, input) {
-    const containerAuth = document.querySelector('.header');
-    const containerContact = document.querySelector('.feedback');
+  // handleOneInputSubmit(typeOfForm, input) {
+  //   const containerAuth = document.querySelector('.header');
+  //   const containerContact = document.querySelector('.feedback');
+  //   const isInvalid = this.validateInput(input);
+  //   let oneInputMessage = 'Это поле должно быть заполнено';
+
+  //   switch (typeOfForm) {
+  //     case 'auth':
+  //       if (isInvalid) {
+  //         this.renderErrorMessage(containerAuth, 'error_auth', oneInputMessage);
+  //         input.closest('.auth__input-wrap').classList.add('auth__input-wrap_error');
+  //       }
+  //       break;
+  //     case 'contact':
+  //       if (isInvalid) {
+  //         this.renderErrorMessage(containerContact, 'error_contact', oneInputMessage);
+  //         input.classList.add('contact-form__input_error');
+  //       }
+  //       break;
+  //   }
+  // }
+
+  handleOneInputSubmit(container, containerErrorClassName, input) {
     const isInvalid = this.validateInput(input);
     let oneInputMessage = 'Это поле должно быть заполнено';
 
-    switch (typeOfForm) {
-      case 'auth':
-        if (isInvalid) {
-          this.renderErrorMessage(containerAuth, 'error_auth', oneInputMessage);
-          input.closest('.auth__input-wrap').classList.add('auth__input-wrap_error');
-        }
-        break;
-      case 'contact':
-        if (isInvalid) {
-          this.renderErrorMessage(containerContact, 'error_contact', oneInputMessage);
-          input.classList.add('contact-form__input_error');
-        }
-        break;
+    if (isInvalid) {
+      this.renderErrorMessage(container, containerErrorClassName, oneInputMessage);
+      input.classList.add('input_type_error');
     }
   }
 
-  handleAllInputsSubmit(typeOfForm, arrOfInputs) {
-    const containerAuth = document.querySelector('.header');
-    const containerContact = document.querySelector('.feedback');
+  handleAllInputsSubmit(container, containerErrorClassName, arrOfInputs) {
     const isAllInvalid = arrOfInputs.every((elem) => this.validateInput(elem) === true);
     const allInputMessage = 'Все поля должны быть заполнены';
 
-    switch (typeOfForm) {
-      case 'auth':
-        if (isAllInvalid) {
-          this.renderErrorMessage(containerAuth, 'error_auth', allInputMessage);
-          arrOfInputs.forEach((elem) => {
-            elem.closest('.auth__input-wrap').classList.add('auth__input-wrap_error');
-          })
-        }
-        break;
-      case 'contact':
-        if (isAllInvalid) {
-          this.renderErrorMessage(containerContact, 'error_contact', allInputMessage);
-          arrOfInputs.forEach((elem) => {
-            elem.classList.add('contact-form__input_error');
-          })
-        }
-        break;
+    if (isAllInvalid) {
+      this.renderErrorMessage(container, containerErrorClassName, allInputMessage);
+      arrOfInputs.forEach((elem) => {
+        elem.classList.add('input_type_error');
+      })
     }
   }
 
@@ -108,18 +106,12 @@ class FormValidation {
 
 export default FormValidation;
 
-export function removeAllErrors(type) {
+export function removeAllErrors() {
   const errors = document.querySelectorAll('.error');
   let errorInputs;
-  let className;
+  let className = 'input_type_error';
 
-  if (type === 'auth') {
-    className = '.auth__input-wrap_error';
-  } else {
-    className = '.contact-form__input_error';
-  }
-
-  errorInputs = document.querySelectorAll(className);
+  errorInputs = document.querySelectorAll(`.${className}`);
 
   if (errors.length > 0) {
     [...errors].forEach((element) => {
@@ -128,7 +120,7 @@ export function removeAllErrors(type) {
   }
 
   if (errorInputs.length > 0) {
-    [...errorInputs].forEach((element) => element.classList.remove(className.slice(1)));
+    [...errorInputs].forEach((element) => element.classList.remove(className));
   }
 
 }
